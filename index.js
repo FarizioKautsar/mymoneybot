7c834de5d668a4458c7894526d77787f13744752
Farizio Kautsar Heruzy <kheruzy@gmail.com>
2021-06-09 08:31:18 +0700

    Finished

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var {Telegraf} = require('telegraf')
 // Insert Telegram bot Token here
 var bot = new Telegraf('1883678744:AAHl333qKEXRQNCXe4qjrpaHu1YLKJubvGM');
 
+var critic = false
+
 // Main welcoming message (/start)
 var welcomeMessage = `
 Halo! Selamat datang di MyMoney. 
@@ -13,19 +15,20 @@ Ada yang dapat MyMoney bantu?
 var menuMessage = `
 Silahkan pilih informasi yang Anda butuhkan.
 
-/konsultasi - Jika ingin mengetahui hal-hal mengenai konsultasi kami
-/daftar - Jika ingin mendaftar menjadi konsultan kami
+Ketik:
+/tentang - Informasi mengenai MyMoney
+/mitra - Informasi mengenai mitra MyMoney
 
-Atau Anda bisa memilih dari menu dibawah ini.
+Atau Anda bisa memilih dari tombol dibawah ini.
 `
 
 var mainMenuInline = {
     inline_keyboard: [
         [
-            {text: 'Informasi Konsultasi', callback_data: 'konsultasi'}
+            {text: 'Tentang Kami', callback_data: 'tentang'}
         ],
         [
-            {text: 'Daftar Sebagai Konsultan', callback_data: 'daftar'}
+            {text: 'Mitra Kami', callback_data: 'mitra'}
         ],
     ]
 }
@@ -38,6 +41,7 @@ bot.start(ctx => {
 })
 
 var menuFunction = ctx => {
+    critic = false
     bot.telegram.sendMessage(ctx.chat.id, menuMessage, {
         reply_markup: mainMenuInline
     })
@@ -47,25 +51,16 @@ bot.command('menu', menuFunction)
 bot.action('menu', menuFunction)
 
 // Konsultasi menu (/konsultasi)
-var konsultasiMessage = `
-Apa yang Anda ingin ketahui mengenai konsultasi kami?
-
-/durasi - Informasi mengenai durasi konsultasi kami
-/topik - Informasi mengenai topik-topik konsultasi kami
+var tentangMessage = `
+MyMoney adalah startup blabla
 
 /menu - Kembali ke menu utama
 `
 
 var konsultasiFunction = ctx => {
-    bot.telegram.sendMessage(ctx.chat.id, konsultasiMessage, {
+    bot.telegram.sendMessage(ctx.chat.id, tentangMessage, {
         reply_markup: {
             inline_keyboard: [
-                [
-                    {text: 'Durasi Konsultasi', callback_data: 'durasi'}
-                ],
-                [
-                    {text: 'Topik Konsultasi', callback_data: 'topik'}
-                ],
                 [
                     {text: 'Kembali', callback_data: 'menu'}
                 ],
@@ -77,67 +72,15 @@ var konsultasiFunction = ctx => {
 bot.command('konsultasi', konsultasiFunction)
 bot.action('konsultasi', konsultasiFunction)
 
-// Konsultasi -> Durasi Konsultasi (/durasi)
-var durasiKonsultasiMessage = `
-Anda akan menerima sesi konsultasi selama X jam setelah pembayaran.
-
-/menu - Kembali ke menu utama
-`
-
-var durasiKonsultasiFunction = ctx => {
-    bot.telegram.sendMessage(ctx.chat.id, durasiKonsultasiMessage, {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {text: 'Kembali', callback_data: 'konsultasi'},
-                    {text: 'Menu Utama', callback_data: 'menu'},
-                ],
-            ]
-        }
-    })
-}
-
-bot.command('durasi', durasiKonsultasiFunction)
-bot.action('durasi', durasiKonsultasiFunction)
-
-// Konsultasi -> Topik Konsultasi (/topik)
-var topikKonsultasiMessage = `
-Mengembangkan bisnis
-
-/menu - Kembali ke menu utama
-`
-
-var topikKonsultasiFunction = ctx => {
-    bot.telegram.sendMessage(ctx.chat.id, topikKonsultasiMessage, {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {text: 'Kembali', callback_data: 'konsultasi'},
-                    {text: 'Menu Utama', callback_data: 'menu'},
-                ],
-            ]
-        }
-    })
-}
-
-bot.command('topik', topikKonsultasiFunction)
-bot.action('topik', topikKonsultasiFunction)
-
 // Daftar sebagai konsultan
-var daftarKonsultanMessage = `
-Langkah untuk mendaftar menjadi konsultan kami:
-1. Kunjungi website kami http://konsulyuk.herokuapp.com/
-2. User membuat akun dengan cara memasukkan data diri melalui formulir yang tersedia di dalam aplikasi/website. User memasukkan detail mengenai kriteria masalah bisnis yang akan di konsultasikan.  
-3. User memilih konsultan.
-4. Diproses terlebih dahulu dan dicarikan referensi yang sesuai dengan masalah bisnis user.  
-5. Penyelesaian transaksi user menyelesaikan transaksi dengan sistem pembayaran yang telah dipilih sebelumnya. Selanjutnya, user akan berkomunikasi dengan konsultan yang tersedia jika sesuai baru menyelesaikan transaksi.
-6. Ada juga fitur artikel dimana terdapat dari referensi-referensi terkait bisnis yang dapat membantu dalam konsultasi bisnis.
+var mitraMessage = `
+MyMoney bekerja sama dengan 
 
 /menu - Kembali ke menu utama
 `
 
-var daftarKonsultanFunction = ctx => {
-    bot.telegram.sendMessage(ctx.chat.id, daftarKonsultanMessage, {
+var mitraFunction = ctx => {
+    bot.telegram.sendMessage(ctx.chat.id, mitraMessage, {
         reply_markup: {
             inline_keyboard: [
                 [
@@ -148,19 +91,21 @@ var daftarKonsultanFunction = ctx => {
     })
 }
 
-bot.command('daftar', daftarKonsultanFunction)
-bot.action('daftar', daftarKonsultanFunction)
+bot.command('mitra', mitraFunction)
+bot.action('mitra', mitraFunction)
 
 // Responses to greetings
 var greetingsMessage = `
-Hello :). Ada pertanyaan tentang Konsul Yuk?
-Silahkan tanyakan informasi yang diperlukan dari pilihan dibawah.
+Halo! Selamat datang di MyMoney. 
+Ada yang dapat MyMoney bantu?
 
-/konsultasi - Jika ingin mengetahui hal-hal mengenai konsultasi kami
-/daftar - Jika ingin mendaftar menjadi konsultan kami
+Silahkan pilih informasi yang Anda butuhkan.
 
-Atau Anda bisa memilih dari menu dibawah ini.
-Selamat mencoba!
+Ketik:
+/tentang - Informasi mengenai MyMoney
+/mitra - Informasi mengenai mitra MyMoney
+
+Atau Anda bisa memilih dari tombol dibawah ini.
 `
 
 bot.hears((/hai|hi|hello|woi|halo|hei|woey|hey/i), ctx => {
@@ -169,28 +114,47 @@ bot.hears((/hai|hi|hello|woi|halo|hei|woey|hey/i), ctx => {
     })
 })
 
-// Error handler unknown command(s)
-var errorMessage = `
-Mohon maaf, saya tidak mengerti perintah Anda.
-Silahkan masukkan perintah berdasarkan informasi yang Anda butuhkan.
-
-/konsultasi - Jika ingin mengetahui hal-hal mengenai konsultasi kami
-/daftar - Jika ingin mendaftar menjadi konsultan kami
-
-Atau Anda bisa memilih dari menu dibawah ini.
+// If user wants to quit, they are required to give critiques and suggestions
+var critiqueMessage = `
+Terima kasih sudah mengunjungi chatbot MyMoney. 
+Silakan berikan kritik & saran Anda kepada kami agar lebih baik.
 `
 
-var critic = false
-bot.hears('kritik', ctx => {
-    critic = !critic
-    ctx.reply('Mo ngomong apa')
+bot.command('quit', ctx => {
+    critic = true
+    ctx.reply(critiqueMessage, {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text: 'Kembali', callback_data: 'menu'}
+                ],
+            ]
+        }
+    })
 })
 
+// Error handler unknown command(s)
+var errorMessage = `
+Mohon maaf, kami tidak mengerti perintah Anda.
+Silahkan pilih informasi yang Anda butuhkan.
 
+Ketik:
+/tentang - Informasi mengenai MyMoney
+/mitra - Informasi mengenai mitra MyMoney
+
+Atau Anda bisa memilih dari tombol dibawah ini.
+`
+
+var quitMessage = `
+Terima kasih atas saran Anda!
+Jangan lupa jaga kesehatan, ya!
+
+Sampai ketemu lagi :)
+`
 bot.on('text', ctx => {
     if (critic) {
-        critic = !critic
-        ctx.reply('anjing')
+        ctx.reply(quitMessage)
+        ctx.leaveChat()
     }
     else {
         bot.telegram.sendMessage(ctx.chat.id, errorMessage, {
@@ -199,5 +163,4 @@ bot.on('text', ctx => {
     }
 })
 
-bot.launch();
 console.log('Bot Launched!')
