891883190aa258937b07a5e8c97b7a483f4b0033
Farizio KHeruzy <kheruzy@gmail.com>
2021-06-10 10:38:41 +0700

    Added tutorial

--- index.js
+++ index.js
@@ -1,194 +1,200 @@
 var {Telegraf} = require('telegraf')
-
 // Insert Telegram bot Token here
 var bot = new Telegraf('1883678744:AAHl333qKEXRQNCXe4qjrpaHu1YLKJubvGM');
+// var session = require('telegraf/session');
 
 var start = true
-var critic = false
 var name = ''
 
-// Main welcoming message (/start)
-var welcomeMessage = `
-Halo! Selamat datang di MyMoney. 
-Dengan siapa kami chattan?
-`
-
-// Main menu
-var menuMessage = (newName) => {
-    name = newName
-    return `Hai ${name}, Ada yang bisa dibantu?`
-}
+// ============================ Starting messages ============================
+var startMessage = name => {
+    return  `Halo ${name}! Selamat datang di MyMoney. Ada yang bisa kami bantu?`
+} 
 
 var mainMenuInline = {
     inline_keyboard: [
         [
+            {text: 'Tutorial Website', callback_data: 'tutorial'}
+        ],
+        [
             {text: 'Tentang MyMoney', callback_data: 'tentang'}
         ],
         [
-            {text: 'Fitur MyMoney', callback_data: 'mitra'}
+            {text: 'Fitur MyMoney', callback_data: 'fitur'}
         ],
         [
-            {text: 'Kendala MyMoney', callback_data: 'keluar'}
+            {text: 'Kendala MyMoney', callback_data: 'kendala'}
         ],
     ]
 }
 
-// Starting reply (/start)
-var startFuncion = ctx => {
-    start = true
-    critic = false
-    ctx.reply(welcomeMessage)
-}
-
-bot.start(startFuncion)
-bot.action('start', startFuncion)
-
-var menuFunction = ctx => {
-    critic = false
-    bot.telegram.sendMessage(ctx.chat.id, menuMessage, {
+var startFunction = ctx => {
+    console.log(ctx.message)
+    ctx.reply(startMessage(ctx.message.from.first_name), {
         reply_markup: mainMenuInline
     })
 }
 
-bot.command('menu', menuFunction)
-bot.action('menu', menuFunction)
+bot.start(startFunction)
 
-// Konsultasi menu (/konsultasi)
-var tentangMessage = `
-MyMoney adalah startup yang menyediakan atau memfasilitasi penggunanya dengan fitur-fitur seperti (M)on(E)y, E-Payment, dan juga Chatbot.
-`
-
-var tentangFunction = ctx => {
-    bot.telegram.sendMessage(ctx.chat.id, tentangMessage, {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {text: 'Kembali', callback_data: 'menu'}
-                ],
-            ]
-        }
-    })
+var menuMessage = name => {
+    if (name) {
+        return `Hai ${name}, ada yang bisa kami bantu?`
+    } else {
+        return `Hai! Ada yang bisa kami bantu?`
+    }
 }
 
-bot.command('tentang', tentangFunction)
-bot.action('tentang', tentangFunction)
-
-// Daftar sebagai konsultan
-var mitraMessage = `
-MyMoney bekerja sama dengan Univesitas Indonesia, Vokasi Universitas Indonesia dan OJK.
-`
-
-var mitraFunction = ctx => {
-    bot.telegram.sendMessage(ctx.chat.id, mitraMessage, {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {text: 'Kembali', callback_data: 'menu'}
-                ],
-            ]
-        }
-    })
-}
-
-bot.command('mitra', mitraFunction)
-bot.action('mitra', mitraFunction)
-
-// Responses to greetings
-var greetingsMessage = `
-Halo! Selamat datang di MyMoney. 
-Ada yang dapat MyMoney bantu?
-
-Silahkan pilih informasi yang Anda butuhkan.
-`
-
-bot.hears((/hai|hi|hello|woi|halo|hei|woey|hey/i), ctx => {
-    bot.telegram.sendMessage(ctx.chat.id, greetingsMessage, {
-        reply_markup: mainMenuInline
-    })
-})
-
-// If user wants to quit, they are required to give critiques and suggestions
-var keluarMessage = `
-Terima kasih sudah mengunjungi chatbot MyMoney. 
-Silakan berikan kritik & saran Anda kepada kami agar lebih baik.
-`
-
-var keluarFunction = ctx => {
-    critic = true
-    bot.telegram.sendMessage(ctx.chat.id, keluarMessage, {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {text: 'Lewati', callback_data: 'finish'}
-                ],
-                [
-                    {text: 'Kembali', callback_data: 'start'}
-                ],
-            ]
-        }
-    })
-}
-
-bot.action('keluar', keluarFunction)
-bot.command('keluar', keluarFunction)
-
-var restartInline = {
-    inline_keyboard: [
-        [
-            {text: 'Mulai Kembali', callback_data: 'start' }
-        ]
-    ]
-}
-
-var finishMessage = `
-Terima kasih sudah menggunakan MyMoney!
-Jangan lupa jaga kesehatan, ya!
-
-Sampai ketemu lagi :)
-`
-bot.action('finish', ctx => {
-    ctx.reply(finishMessage, {
-        reply_markup: restartInline
-    })
-})
-
-// Error handler unknown command(s)
 var errorMessage = `
 Mohon maaf, kami tidak mengerti perintah Anda.
 Silahkan pilih informasi yang Anda butuhkan.
 `
 
-var quitMessage = `
-Terima kasih atas saran Anda!
-Jangan lupa jaga kesehatan, ya!
-
-Sampai ketemu lagi :)
-`
-
 bot.on('text', ctx => {
     if (start) {
         start = false
-        bot.telegram.sendMessage(ctx.chat.id, menuMessage(ctx.message.text), {
+        ctx.reply(menuMessage(ctx.message.from.first_name), {
             reply_markup: mainMenuInline
         })
     }
     else {
-        if (critic) {
-            critic = false
-            ctx.reply(quitMessage, {
-                reply_markup: restartInline
-            })
-        }
-        else {
-            bot.telegram.sendMessage(ctx.chat.id, errorMessage, {
-                reply_markup: mainMenuInline
-            })
-        }
+        ctx.reply(errorMessage, {
+            reply_markup: mainMenuInline
+        })
     }
 })
 
-bot.launch()
-console.log('Bot Launched!')
+var menuFunction = ctx => {
+    // console.log(ctx.message)
+    ctx.reply(menuMessage(null), {
+        reply_markup: mainMenuInline
+    })
+}
 
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+bot.action('menu', menuFunction)
+bot.command('menu', menuFunction)
+
+var backInline = {
+    inline_keyboard: [
+        [
+            {text: 'Kembali', callback_data: 'menu'}
+        ]
+    ]
+}
+
+// ============================ Tutorial Website ============================
+var tutorialMessage = `
+1. Membuat akun dengan menekan tombol LOGIN, kemudian slide halaman ke bawah
+2. Isi data sesuai dengan yang dibutuhkan di Website
+3. Data sudah terdaftar, kemudia login username dan password
+4. Website sudah bisa digunakan☺️
+`
+
+var tutorialFunction = ctx => {
+    ctx.reply(tutorialMessage, {
+        reply_markup: backInline
+    })
+}
+
+bot.action('tutorial', tutorialFunction)
+bot.command('tutorial', tutorialFunction)
+
+// ============================ Tentang MyMoney ============================
+var tentangMessage = `
+MyMoney merupakan website e-transaction baik sesama akun MyMoney, transfer ATM, pembayaran pulsa HP, pembayaran Listrik, dan pembayaran telp yang mudah dan aman.
+`
+
+var tentangFunction = ctx => {
+    ctx.reply(tentangMessage, {
+        reply_markup: backInline
+    })
+}
+
+bot.action('tentang', tentangFunction)
+bot.command('tentang', tentangFunction)
+
+
+// ============================ Fitur MyMoney ============================
+var fiturMessage = `
+Fitur MyMoney adalah sebagai berikut:
+`
+
+var fiturFunction = ctx => {
+    ctx.reply(fiturMessage, {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text: 'E-Payment', callback_data: 'epayment'}
+                ],
+                [
+                    {text: 'Personal-E', callback_data: 'personale'}
+                ],
+                [
+                    {text: 'Kembali', callback_data: 'menu'}
+                ]
+            ]
+        }
+    })
+}
+
+bot.action('fitur', fiturFunction)
+bot.command('fitur', fiturFunction)
+
+
+// ============================ Fitur -> E-Payment ============================
+var epaymentMessage = `
+E-Payment merupakan salah satu fitu kami, fitur ini dapat memudahkan para pengguna dalam melakukan transaksi antarakun, antar bank, dan dapat melakukan pembayaran.
+`
+
+var epaymentFunction = ctx => {
+    ctx.reply(epaymentMessage, {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text: 'Kembali', callback_data: 'fitur'}
+                ]
+            ]
+        }
+    })
+}
+
+bot.action('epayment', epaymentFunction)
+bot.command('epayment', epaymentFunction)
+
+
+// ============================ Fitur -> Personal-E ============================
+var personaleMessage = `
+Personal-E merupkan salah satu fitur MyMoney, fitur ini merupakan chatbot yang dapat membantu penggunanya dalam menggunakan atau pengoperasi Website MyMoney
+`
+
+var personaleFunction = ctx => {
+    ctx.reply(personaleMessage, {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text: 'Kembali', callback_data: 'fitur'}
+                ]
+            ]
+        }
+    })
+}
+
+bot.action('personale', personaleFunction)
+bot.command('personale', personaleFunction)
+
+
+// ============================ Kendala MyMoney ============================
+var kendalaMessage = `
+Anda bisa mengadukan kendala dengan mengirimkan email ke my.moneyyyy@gmail.com
+`
+
+var kendalaFunction = ctx => {
+    ctx.reply(kendalaMessage, {
+        reply_markup: backInline
+    })
+}
+
+bot.action('kendala', kendalaFunction)
+bot.command('kendala', kendalaFunction)
+
+bot.launch()
